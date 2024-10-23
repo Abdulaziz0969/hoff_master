8639df4ca1045aeea29574b1fc517c7da4910b81
Abdulaziz <@abdulaziz08262005@gmail.com>
2024-10-23 11:56:35 +0500

    project  added to github

---
+++ lib/SplashScreen.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:hoff_master/Home_Page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => LanguageSelectionScreen(),
+            )));
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        alignment: Alignment.center,
+        child: Text(
+          "Hoff",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
